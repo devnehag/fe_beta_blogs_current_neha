1bd95c71b3fc80b9c60c7faf916ce8e0d381081f
Neha Gupta <gupta.neha2004@gmail.com>
2024-03-19 04:46:49 -0700

    blog likes notifications implemented

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: false, 
-  apiUrl: "http://localhost:3000"
+  apiUrl: "http://localhost:3000",
+  pusher:{
+    key: "0497f0a5fc5b213d1d73",
+    cluster: "ap2"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true, 
-  apiUrl: "https://betablogsgf.onrender.com"
+  apiUrl: "https://betablogsgf.onrender.com",
+  pusher:{
+    key: "0497f0a5fc5b213d1d73",
+    cluster: "ap2"
+  },
 };
